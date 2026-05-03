26ccf948817c4533246b423117025bb3d9ae4f84
Kamlesh kumar <160771793+kamleshkumarr@users.noreply.github.com>
2026-05-03 18:19:30 +0530

    Restore Django settings in settings.py

--- productivity_ai/settings.py
+++ productivity_ai/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'accounts.middleware.NoCacheMiddleware', 
+    'accounts.middleware.NoCacheMiddleware', 
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
